0b536244d6a0d4959f075739b13295aebbb6acff
ivyie1234 <osmerahyvonne@gmail.com>
2024-05-16 03:22:41 -0700

    j

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,6 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
 
-
-
-        refs/remotes/origin/master
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
 
