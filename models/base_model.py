d226d1bff922828560d8c9f22da0798e724397ac
Obarechrispus <obarechrispus@gmail.com>
2024-05-16 21:46:52 +0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,19 @@ from datetime import datetime
 class BaseModel:
 
     def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ['created_at', 'updated_at']:
-                        continue
-                    elif key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value, time_format))
-                    else:
-                        setattr(self, key, value)
+                if key == "__class__":
+                    #if key in ['created_at', 'updated_at']:
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
+
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
 
