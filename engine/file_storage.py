ff1733ca9fa0194892f1e5a3846d62e0319d9d97
Obarechrispus <obarechrispus@gmail.com>
2024-05-19 20:20:25 +0300

    update

---
+++ engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+import json
+from models.base_model import BaseModel
+from models.user import User
+
+
+class FileStorage:
+    """
+    
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        
+        """
+        return self.__objects
+
+    def new(self, object):
+        """
+        """
+        self.__objects[object.__class__.__name__ + '.' + str(object)] = object
+
+    def save(self):
+        """
+    
+        """
+        with open(self.__file_path, 'w+') as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()
+                       }, f)
+
+    def reload(self):
+        """
+    
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                dict = json.loads(f.read())
+                for value in dict.values():
+                    cls = value["__class__"]
+                    self.new(eval(cls)(**value))
+        except Exception:
+            pass
